4a4f73c0900caec355c3530437c314c6e9bfff06
Hiroya Kubo <hiroya@cuc.ac.jp>
2021-07-11 08:00:28 +0900

    support Java Native Access wrapper

---
+++ ApspTest.java
@@ -0,0 +1,261 @@
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+public class ApspTest {
+
+    public interface ApspLibrary extends Library {
+        public void nop();
+
+        public void floyd_warshall_blocked(int[] input, int[] output, int[] parents, int n, int b);
+
+        public void floyd_warshall_blocked_float(float[] input, float[] output, int[] parents, int n, int b);
+
+        public void floyd_warshall_blocked_double(double[] input, double[] output, int[] parents, int n, int b);
+
+        public void johnson_parallel_matrix(int[] input, int[] output, int[] parents, int n);
+
+        public void johnson_parallel_matrix_float(float[] input, float[] output, int[] parents, int n);
+
+        public void johnson_parallel_matrix_double(double[] input, double[] output, int[] parents, int n);
+    }
+
+    public interface ApspSeq extends ApspLibrary {
+        ApspSeq INSTANCE = Native.load("apsp-seq", ApspSeq.class);
+    }
+
+    public interface ApspSeqIspc extends ApspLibrary {
+        ApspSeqIspc INSTANCE = Native.load("apsp-seq-ispc", ApspSeqIspc.class);
+    }
+
+    public interface ApspOmp extends ApspLibrary {
+        ApspOmp INSTANCE = Native.load("apsp-omp", ApspOmp.class);
+    }
+
+    public interface ApspOmpIspc extends ApspLibrary {
+        ApspOmpIspc INSTANCE = Native.load("apsp-omp-ispc", ApspOmpIspc.class);
+    }
+
+    static final int INT_INF = 1073741823;
+    static final float FLT_INF = 3.402823466e+37f;
+    static final double DBL_INF = 1.7976931348623158e+307;
+
+    public static void print_int(int value) {
+        System.out.print(value == INT_INF ? "Inf" : value);
+    }
+
+    public static void print_float(float value) {
+        System.out.print(value == FLT_INF ? "Inf" : value);
+    }
+
+    public static void print_double(double value) {
+        System.out.print(value == DBL_INF ? "Inf" : value);
+    }
+
+    public static void print_matrix_int(int[] matrix, int n) {
+        for (int i = 0; i < n; i++) {
+            print_int(matrix[i * n]);
+            for (int j = 1; j < n; j++) {
+                System.out.print(", ");
+                print_int(matrix[i * n + j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void print_matrix_float(float[] matrix, int n) {
+        for (int i = 0; i < n; i++) {
+            print_float(matrix[i * n]);
+            for (int j = 1; j < n; j++) {
+                System.out.print(", ");
+                print_float(matrix[i * n + j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void print_matrix_double(double[] matrix, int n) {
+        for (int i = 0; i < n; i++) {
+            print_double(matrix[i * n]);
+            for (int j = 1; j < n; j++) {
+                System.out.print(", ");
+                print_double(matrix[i * n + j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static void exec_int(int[] input, int n, String algorithm) {
+        int[] output = new int[n * n];
+        int[] parents = new int[n * n];
+
+        long timeStart = System.currentTimeMillis();
+
+        switch (algorithm) {
+            case "seq-f":
+                ApspSeq.INSTANCE.floyd_warshall_blocked(input, output, parents, n, 1);
+                break;
+            case "seq-ispc-f":
+                ApspSeqIspc.INSTANCE.floyd_warshall_blocked(input, output, parents, n, 1);
+                break;
+            case "omp-f":
+                ApspOmp.INSTANCE.floyd_warshall_blocked(input, output, parents, n, 1);
+                break;
+            case "omp-ispc-f":
+                ApspOmpIspc.INSTANCE.floyd_warshall_blocked(input, output, parents, n, 1);
+                break;
+            case "seq-j":
+                ApspSeq.INSTANCE.johnson_parallel_matrix(input, output, parents, n);
+                break;
+            case "seq-ispc-j":
+                ApspSeqIspc.INSTANCE.johnson_parallel_matrix(input, output, parents, n);
+                break;
+            case "omp-j":
+                ApspOmp.INSTANCE.johnson_parallel_matrix(input, output, parents, n);
+                break;
+            case "omp-ispc-j":
+                ApspOmpIspc.INSTANCE.johnson_parallel_matrix(input, output, parents, n);
+                break;
+            default:
+                ApspSeq.INSTANCE.nop();
+                break;
+        }
+        long timeEnd = System.currentTimeMillis();
+
+        System.out.println("Finished in " + (timeEnd - timeStart) + " ms");
+
+        System.out.println("[input]");
+        print_matrix_int(input, n);
+        System.out.println("[output]");
+        print_matrix_int(output, n);
+        System.out.println("[parents]");
+        print_matrix_int(parents, n);
+    }
+
+    static void exec_float(float[] input, int n, String algorithm) {
+        float[] output = new float[n * n];
+        int[] parents = new int[n * n];
+
+        long timeStart = System.currentTimeMillis();
+        switch (algorithm) {
+            case "seq-f":
+                ApspSeq.INSTANCE.floyd_warshall_blocked_float(input, output, parents, n, 1);
+                break;
+            case "seq-ispc-f":
+                ApspSeqIspc.INSTANCE.floyd_warshall_blocked_float(input, output, parents, n, 1);
+                break;
+            case "omp-f":
+                ApspOmp.INSTANCE.floyd_warshall_blocked_float(input, output, parents, n, 1);
+                break;
+            case "omp-ispc-f":
+                ApspOmpIspc.INSTANCE.floyd_warshall_blocked_float(input, output, parents, n, 1);
+                break;
+            case "seq-j":
+                ApspSeq.INSTANCE.johnson_parallel_matrix_float(input, output, parents, n);
+                break;
+            case "seq-ispc-j":
+                ApspSeqIspc.INSTANCE.johnson_parallel_matrix_float(input, output, parents, n);
+                break;
+            case "omp-j":
+                ApspOmp.INSTANCE.johnson_parallel_matrix_float(input, output, parents, n);
+                break;
+            case "omp-ispc-j":
+                ApspOmpIspc.INSTANCE.johnson_parallel_matrix_float(input, output, parents, n);
+                break;
+        }
+        long timeEnd = System.currentTimeMillis();
+
+        System.out.println("Finished in " + (timeEnd - timeStart) + " ms");
+
+        System.out.println("[input]");
+        print_matrix_float(input, n);
+        System.out.println("[output]");
+        print_matrix_float(output, n);
+        System.out.println("[parents]");
+        print_matrix_int(parents, n);
+    }
+
+    static void exec_double(double[] input, int n, String algorithm) {
+        double[] output = new double[n * n];
+        int[] parents = new int[n * n];
+
+        long timeStart = System.currentTimeMillis();
+        switch (algorithm) {
+            case "seq-f":
+                ApspSeq.INSTANCE.floyd_warshall_blocked_double(input, output, parents, n, 1);
+                break;
+            case "seq-ispc-f":
+                ApspSeqIspc.INSTANCE.floyd_warshall_blocked_double(input, output, parents, n, 1);
+                break;
+            case "omp-f":
+                ApspOmp.INSTANCE.floyd_warshall_blocked_double(input, output, parents, n, 1);
+                break;
+            case "omp-ispc-f":
+                ApspOmpIspc.INSTANCE.floyd_warshall_blocked_double(input, output, parents, n, 1);
+                break;
+            case "seq-j":
+                ApspSeq.INSTANCE.johnson_parallel_matrix_double(input, output, parents, n);
+                break;
+            case "seq-ispc-j":
+                ApspSeqIspc.INSTANCE.johnson_parallel_matrix_double(input, output, parents, n);
+                break;
+            case "omp-j":
+                ApspOmp.INSTANCE.johnson_parallel_matrix_double(input, output, parents, n);
+                break;
+            case "omp-ispc-j":
+                ApspOmpIspc.INSTANCE.johnson_parallel_matrix_double(input, output, parents, n);
+                break;
+        }
+        long timeEnd = System.currentTimeMillis();
+
+        System.out.println("Finished in " + (timeEnd - timeStart) + " ms");
+
+        System.out.println("[input]");
+        print_matrix_double(input, n);
+        System.out.println("[output]");
+        print_matrix_double(output, n);
+        System.out.println("[parents]");
+        print_matrix_int(parents, n);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("java JNATest [seq-f|seq-ispc-f|omp-f|omp-ispc-f|seq-j|seq-ispc-j|omp-j|omp-ispc-j] [i|f|d]");
+            System.exit(0);
+        }
+        String algorithm = args[0];
+        String type = args[1];
+        int n = 5;
+        int[] input_i = {
+                0, 10, 200, 30, 40,
+                10, 0, 30, 100, 100,
+                20, 30, 0, 40, 50,
+                30, 100, 40, 0, 60,
+                40, 100, 50, 60, 0,
+        };
+        float[] input_f = {
+                0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+        };
+        double[] input_d = {
+                0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 0.0,
+                0.0, 0.0, 0.0, 0.0, 0.0
+        };
+        switch (type) {
+            case "i":
+                exec_int(input_i, n, algorithm);
+                break;
+            case "f":
+                exec_float(input_f, n, algorithm);
+                break;
+            case "d":
+                exec_double(input_d, n, algorithm);
+                break;
+        }
+    }
+}
--- Makefile
+++ Makefile
@@ -4,9 +4,12 @@ UNAME = $(shell uname)
 CXX ?= g++
 CXXFLAGS ?= -std=c++11 -Wall -Wextra -g -fPIC -O3
 CXXFLAGS += $(CXXEXTRA)
-LDFLAGS ?=
+LDFLAGS ?= -L./libs
 
-SHAREDFLAGS ?= -shared -fPIC
+SHAREDFLAGS ?= -shared -fPIC -dynamiclib
+
+CLASSPATH := jna-5.8.0.jar:jna-platform-5.8.0.jar:.
+JAVA_OPT := -Djna.library.path=./libs
 
 ISPC ?= ispc
 ISPC_FLAGS ?= --arch=x86-64 --emit-obj -g -03
@@ -29,11 +32,11 @@ CPP_SOURCES := $(shell find $(SRC_DIR) -name '*cpp')
 CU_SOURCES := $(shell find $(SRC_DIR) -name '*cu')
 ISPC_SOURCES := $(shell find $(SRC_DIR) -name '*ispc')
 
-SEQ_LIB := $(LIBS_DIR)/libapsp-seq.so
-OMP_LIB := $(LIBS_DIR)/libapsp-omp.so
-CUDA_LIB := $(LIBS_DIR)/libapsp-cuda.so
-SEQ_ISPC_LIB := $(LIBS_DIR)/libapsp-seq-ispc.so
-OMP_ISPC_LIB := $(LIBS_DIR)/libapsp-omp-ispc.so
+SEQ_LIB := $(LIBS_DIR)/libapsp-seq.dylib
+OMP_LIB := $(LIBS_DIR)/libapsp-omp.dylib
+CUDA_LIB := $(LIBS_DIR)/libapsp-cuda.dylib
+SEQ_ISPC_LIB := $(LIBS_DIR)/libapsp-seq-ispc.dylib
+OMP_ISPC_LIB := $(LIBS_DIR)/libapsp-omp-ispc.dylib
 
 SEQ_OBJECTS := $(CPP_SOURCES:$(SRC_DIR)/%.cpp=$(OBJ_DIR)/seq-%.o)
 OMP_OBJECTS := $(CPP_SOURCES:$(SRC_DIR)/%.cpp=$(OBJ_DIR)/omp-%.o)
@@ -55,17 +58,22 @@ ISPC_SEQ_LIB_OBJECTS := $(filter-out $(OBJ_DIR)/ispc-seq-main.o, $(ISPC_SEQ_OBJE
 ISPC_OMP_LIB_OBJECTS := $(filter-out $(OBJ_DIR)/ispc-omp-main.o, $(ISPC_OMP_OBJECTS))
 ISPC_LIB_OBJECTS := $(filter-out $(OBJ_DIR)/ispc-main.o, $(ISPC_OBJECTS))
 
+JAVA_SRCS := ApspTest.java
+JAVA_CLASS := ApspTest.class
+
 ifeq ($(UNAME), Linux)
 $(SEQ_ISPC) $(OMP_ISPC): ISPCFLAGS += --target=avx2-i32x8
 $(OMP) $(OMP_ISPC) $(OMP_LIB) $(OMP_ISPC_LIB): LDFLAGS += -L/usr/lib/llvm-12/lib -Xpreprocessor -fopenmp -lomp 
 $(CUDA): NVCCFLAGS += -arch=compute_61 -code=sm_61 --compiler-options "-fPIC" 
 $(CUDA) $(CUDA_LIB): CXXFLAGS += -DCUDA
-$(CUDA) $(CUDA_LIB): LDFLAGS += -DCUDA -L/usr/lib/llvm-12/lib -L/usr/local/cuda/lib64 -lcudart -lomp 
-all: $(SEQ) $(OMP) $(CUDA) $(SEQ_ISPC) $(OMP_ISPC) $(SEQ_LIB) $(OMP_LIB) $(CUDA_LIB) $(SEQ_ISPC_LIB) $(OMP_ISPC_LIB)
+$(CUDA) $(CUDA_LIB): LDFLAGS += -DCUDA -L/usr/lib/llvm-12/lib -L/usr/local/cuda/lib64 -lcudart -lomp
+LIBS := $(SEQ_LIB) $(OMP_LIB) $(CUDA_LIB) $(SEQ_ISPC_LIB) $(OMP_ISPC_LIB)
+all: $(SEQ) $(OMP) $(CUDA) $(SEQ_ISPC) $(OMP_ISPC) $(LIBS) $(JAVA_CLASS)
 else
 # CXXFLAGS += -I/opt/boost-1.61.0/include -I$(SRC_DIR)/
 #$(OMP) $(OMP_ISPC) $(CUDA): CXXFLAGS += -Xpreprocessor -fopenmp
-all: $(SEQ) $(OMP) $(SEQ_ISPC) $(OMP_ISPC) $(SEQ_LIB) $(OMP_LIB) $(SEQ_ISPC_LIB) $(OMP_ISPC_LIB)
+LIBS := $(SEQ_LIB) $(OMP_LIB) $(SEQ_ISPC_LIB) $(OMP_ISPC_LIB)
+all: $(SEQ) $(OMP) $(SEQ_ISPC) $(OMP_ISPC) $(LIBS) $(JAVA_CLASS)
 endif
 
 $(SEQ_ISPC) $(OMP_ISPC): CXXFLAGS += -DISPC
@@ -75,21 +83,20 @@ $(OBJ_DIR):
 $(LIBS_DIR):
 	mkdir -p $@
 
-$(SEQ): $(SEQ_OBJECTS)
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) $^ -o $@
+$(SEQ): $(SEQ_OBJECTS) $(SEQ_LIB) $(OBJ_DIR)/seq-main.o
+	$(CXX) $(CXXFLAGS) $(OBJ_DIR)/seq-main.o -o $@ $(LDFLAGS) -lapsp-seq
 
-$(OMP): $(OMP_OBJECTS)
-	$(CXX) $(CXXFLAGS) $^ -o $@ $(LDFLAGS) 
+$(OMP): $(OMP_OBJECTS) $(OMP_LIB) $(OBJ_DIR)/omp-main.o
+	$(CXX) $(CXXFLAGS) $(OBJ_DIR)/omp-main.o -o $@ $(LDFLAGS) -lapsp-omp
 
-$(CUDA): $(CUDA_CPP_OBJECTS) $(CUDA_CU_OBJECTS)
-	$(CXX) $(CXXFLAGS) $^ -o $@ $(LDFLAGS)
+$(CUDA): $(CUDA_CPP_OBJECTS) $(CUDA_CU_OBJECTS) $(CUDA_LIB) $(OBJ_DIR)/cuda-cpp-main.o
+	$(CXX) $(CXXFLAGS) $(OBJ_DIR)/cuda-cpp-main.o -o $@ $(LDFLAGS) -lapsp-cuda
 
-$(SEQ_ISPC): $(ISPC_SEQ_OBJECTS) $(ISPC_OBJECTS)
-	$(CXX) $(CXXFLAGS) $^ -o $@ $(LDFLAGS)
+$(SEQ_ISPC): $(ISPC_SEQ_OBJECTS) $(ISPC_OBJECTS) $(SEQ_ISPC_LIB) $(OBJ_DIR)/ispc-seq-main.o
+	$(CXX) $(CXXFLAGS) $(OBJ_DIR)/ispc-seq-main.o -o $@ $(LDFLAGS) -lapsp-seq-ispc
 
-$(OMP_ISPC): $(ISPC_OMP_OBJECTS) $(ISPC_OBJECTS)
-	$(CXX) $(CXXFLAGS) $^ -o $@ $(LDFLAGS)
-
+$(OMP_ISPC): $(ISPC_OMP_OBJECTS) $(ISPC_OBJECTS) $(OMP_ISPC_LIB) $(OBJ_DIR)/ispc-omp-main.o
+	$(CXX) $(CXXFLAGS) $(OBJ_DIR)/ispc-omp-main.o -o $@ $(LDFLAGS) -lapsp-omp-ispc
 
 $(SEQ_LIB): $(SEQ_LIB_OBJECTS) | $(LIBS_DIR)
 	$(CXX) $(SHAREDFLAGS) $(CXXFLAGS) $^ -o $@ $(LDFLAGS)
@@ -137,3 +144,10 @@ clean:
 	$(RM) -r $(OBJ_DIR) $(LIBS_DIR)
 	$(RM) $(SEQ) $(OMP) $(CUDA) $(SEQ_ISPC) $(OMP_ISPC) $(SEQ_LIB) $(OMP_LIB) $(CUDA_LIB) $(SEQ_ISPC_LIB) $(OMP_ISPC_LIB)
 
+$(JAVA_CLASS): $(LIBS) $(JAVA_SRCS)
+	javac -cp $(CLASSPATH) $(JAVA_SRCS)
+
+ApspTest: $(JAVA_CLASS)
+	java -cp $(CLASSPATH) $(JAVA_OPT) ApspTest
+
+run: ApspTest
