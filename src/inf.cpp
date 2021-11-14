69058e7c61c3a5932cab03f034d8f96203719c8b
Hiroya Kubo <hiroya@cuc.ac.jp>
2021-11-15 08:15:17 +0900

    add getInfinity functions to extern

---
+++ src/inf.cpp
@@ -0,0 +1,12 @@
+#include "inf.hpp"
+#include<string>
+
+int getIntegerInfinity(){
+  return INT_INF;
+}
+float getFloatInfinity(){
+  return FLT_INF;
+}
+double getDoubleInfinity(){
+  return DBL_INF;
+}
--- src/inf.hpp
+++ src/inf.hpp
@@ -1,6 +1,10 @@
 
 #define INT_INF std::numeric_limits<int>::max() / 2
 
-#define FLT_INF std::numeric_limits<float>::infinity() / 2
+#define FLT_INF std::numeric_limits<float>::max() / 2
 
-#define DBL_INF std::numeric_limits<double>::infinity() / 2
+#define DBL_INF std::numeric_limits<double>::max() / 2
+
+extern "C" int getIntegerInfinity();
+extern "C" float getFloatInfinity();
+extern "C" double getDoubleInfinity();
