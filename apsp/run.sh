d2c619552fe723364604d65b0c7ddca4e47a040f
Hiroya Kubo <hiroya@cuc.ac.jp>
2022-01-20 10:17:25 +0900

    upgrade jna 5.10.0

--- apsp/run.sh
+++ apsp/run.sh
@@ -1,3 +1,3 @@
-java -Xmx16g -cp target/classes:$HOME/.m2/repository/net/java/dev/jna/jna/5.8.0/jna-5.8.0.jar:$HOME/.m2/repository/net/java/dev/jna/jna-platform/5.8.0/jna-platform-5.8.0.jar -Djna.library.path=$HOME/APSP-in-parallel/apsp/target/classes org.idegsm.AllPairsShortestPathsTestResolver $1 $2 $3
+java -Xmx16g -cp target/classes:$HOME/.m2/repository/net/java/dev/jna/jna/5.10.0/jna-5.10.0.jar:$HOME/.m2/repository/net/java/dev/jna/jna-platform/5.10.0/jna-platform-5.10.0.jar -Djna.library.path=$HOME/APSP-in-parallel/apsp/target/classes org.idegsm.AllPairsShortestPathsTestResolver $1 $2 $3
 
 
